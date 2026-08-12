59815a91c9259f62929d461b1e4355dcc1000e8b
syedahansabibi-web <syedahansabibi@gmail.com>
2026-08-12 23:52:28 +0500

    Add files via upload

--- sitemap.ts
+++ sitemap.ts
@@ -1,25 +1,21 @@
 
-import { MetadataRoute } from "next";
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://home-puce-kappa.vercel.app";
+  <url>
+    <loc>https://home-puce-kappa.vercel.app/</loc>
+  </url>
 
-  return [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/about`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/privacy-policy`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/contact`,
-      lastModified: new Date(),
-    },
-  ];
-}
+  <url>
+    <loc>https://home-puce-kappa.vercel.app/about</loc>
+  </url>
+
+  <url>
+    <loc>https://home-puce-kappa.vercel.app/privacy-policy</loc>
+  </url>
+
+  <url>
+    <loc>https://home-puce-kappa.vercel.app/contact</loc>
+  </url>
+
+</urlset>
