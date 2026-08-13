805e88906b6570a1b868678e25118f08a073628d
syedahansabibi-web <syedahansabibi@gmail.com>
2026-08-13 05:46:20 +0500

    Add files via upload

--- sitemap.ts
+++ sitemap.ts
@@ -1,1 +1,13 @@
 
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://home-puce-kappa.vercel.app";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+  ];
+}
