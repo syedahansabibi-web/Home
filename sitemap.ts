301742fa6961332c9684c51fa79376dd75fa628e
syedahansabibi-web <syedahansabibi@gmail.com>
2026-08-13 06:13:08 +0500

    Add files via upload

--- sitemap.ts
+++ sitemap.ts
@@ -1,1 +1,17 @@
+type MetadataRoute = {
+  Sitemap: Array<{
+    url: string;
+    lastModified?: Date | string;
+  }>;
+};
 
+export default function sitemap(): MetadataRoute["Sitemap"] {
+  const baseUrl = "https://home-puce-kappa.vercel.app";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+  ];
+}
